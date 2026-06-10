21de590444e4ba59f179601d0a3d34fb360a0405
Abdelrahman-Dev9 <abdoelnagar198@gmail.com>
2026-06-10 23:01:50 +0300

    protecting routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Admins from "./pages/Admins";
+import CreateNewPassword from "./pages/auth/CreateNewPassword";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import Login from "./pages/auth/Login";
 import OtpVerification from "./pages/auth/OtpVerification";
 import Dashboard from "./pages/Dashboard";
+import Notifications from "./pages/Notifications";
+import Profile from "./pages/Profile";
+import Reports from "./pages/Reports";
 import TrackingList from "./pages/TrackingList";
 import Users from "./pages/Users";
-import Admins from "./pages/Admins";
-import Notifications from "./pages/Notifications";
-import Reports from "./pages/Reports";
-import CreateNewPassword from "./pages/auth/CreateNewPassword";
-import Profile from "./pages/Profile";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import PublicRoute from "./routes/PublicRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/otp-verification" element={<OtpVerification />} />
-        <Route path="/create-new-password" element={<CreateNewPassword />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tracking" element={<TrackingList />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/admins" element={<Admins />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/otp-verification" element={<OtpVerification />} />
+          <Route path="/create-new-password" element={<CreateNewPassword />} />
+        </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tracking" element={<TrackingList />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/admins" element={<Admins />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
